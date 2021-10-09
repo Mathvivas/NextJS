bb753b31dc409d797ff30e8cdd29c19f2d36ebaf
Mathvivas <mathvivas@hotmail.com>
2021-10-09 15:03:38 -0300

    Inicio dos Fundamentos

---
+++ fundamentos-nextjs/pages/artigos/programacao-com-java.js
@@ -0,0 +1,7 @@
+const DetalhesPage = () => {
+    return (
+        <h1>Detalhes sobre Java</h1>
+    )
+}
+
+export default DetalhesPage
---
+++ fundamentos-nextjs/pages/index.js
@@ -0,0 +1,8 @@
+// host:porta/
+const PrincipalPage = () => {
+	return (
+		<h1>Página Principal</h1>
+	)
+}
+
+export default PrincipalPage
