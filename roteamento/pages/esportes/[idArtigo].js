3044485c2a14798f3c4229733f42d82588f55ec8
Mathvivas <mathvivas@hotmail.com>
2021-10-19 11:28:33 -0300

    Roteamento de Paginas

---
+++ roteamento/pages/esportes/[idArtigo].js
@@ -0,0 +1,10 @@
+import { useRouter } from 'next/router'
+
+const ArtigoEsportesPage = () => {
+    const router = useRouter()
+    return (
+        <p>{router.query.idArtigo}</p>
+    )
+}
+
+export default ArtigoEsportesPage
