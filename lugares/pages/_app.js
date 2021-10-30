d249e37a29524f972c3d8e56a7428a4fbf61fcfd
Mathvivas <mathvivas@hotmail.com>
2021-10-30 14:57:08 -0300

    Construçao dos Componentes e do Forms

---
+++ lugares/pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+// Escolha o Tema que Desejar
+// https://primefaces.org/primereact/showcase/#/setup
+import 'primereact/resources/themes/bootstrap4-dark-purple/theme.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
